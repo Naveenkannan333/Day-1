5f84b2be6dbc9bbebdb5e17a41b8e03535fef09b
asus <asus@DESKTOP-H9HOAD8>
2022-02-08 15:47:24 +0530

    First Commit

---
+++ Day1/src/com/Habile/Sample.java
@@ -0,0 +1,21 @@
+package com.Habile;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class TestArrayLinked{    
+	 public static void main(String args[]){    
+	     
+	  List<String> al=new ArrayList<String>();   
+	  al.add("Vijay");    
+	  al.add("Ravi");    
+	    
+	  List<String> al2=new LinkedList<String>();    
+	  al2.add("Swati");    
+	  al2.add("Junaid");    
+	    
+	  System.out.println("arraylist: "+al);  
+	  System.out.println("linkedlist: "+al2);  
+	 }    
+	}
